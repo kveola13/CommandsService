2abc317dbc142ec223386c89977c6cc1cace7780
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-15 08:57:24 +0100

    Add Test Actionresult

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -2,7 +2,20 @@
 
 namespace CommandsService.Controllers
 {
+    [Route("api/c/[controller]")]
+    [ApiController]
     public class PlatformsController : ControllerBase
     {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("Inbound connection");
+            return Ok("Inbound test from Platforms Controller");
+        }
     }
 }
