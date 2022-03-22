52167c1c2ae17ea360e2fee9a3df316731293fd5
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2022-03-22 09:17:28 +0100

    update appdb

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                .HasMany(platform => platform.Commands)
+                .WithOne(platform => platform.Platform!)
+                .HasForeignKey(platform => platform.PlatformId);
+            modelBuilder.Entity<Command>()
+                .HasOne(command => command.Platform)
+                .WithMany(command => command.Commands)
+                .HasForeignKey(command => command.PlatformId);
+        }
+    }
+}
